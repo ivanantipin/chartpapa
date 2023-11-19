2738efae4a0409fcc952419cf5ab406f8a6c0b77
ivanantipin <ivan.antipin@gmail.com>
2023-11-19 18:41:09 +0100

    rest report

--- techbot/src/main/kotlin/com/firelib/techbot/command/IndicatorCommand.kt
+++ techbot/src/main/kotlin/com/firelib/techbot/command/IndicatorCommand.kt
@@ -25,7 +25,7 @@ class IndicatorCommand(val techBotApp: TechbotApp) {
         val ohlcs = techBotApp.ohlcService().getOhlcsForTf(instrId, tf.interval)
         val hOptions = signalGenerator.drawPicture(instrId, tf, settings, ohlcs)
         val bytes = ChartService.post(hOptions)
-        bot.sendMessage(chatId = update.chatId(), "#${hOptions.title?.text}")
+        bot.sendMessage(chatId = update.chatId(), "#${instrId.code}")
         bot.sendPhoto(chatId = update.chatId(), TelegramFile.ByByteArray(bytes))
     }
 }
